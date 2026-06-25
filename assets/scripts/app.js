3b75d8c442fffbcf0426935ac4ca2ae68dce0b14
nency rana <nency@weetechsolution.com>
2026-06-25 13:16:30 +0530

    create default shopping cart and shop class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,16 +14,33 @@ class Product {
   }
 }
 
+class ShoppingCart {
+  items = [];
+
+  render() {
+    const cartEl = document.createElement("selection");
+    cartEl.innerHTML = `
+      <h2>Total : \$${0} </h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    return cartEl;
+  }
+}
 class ProductItem {
   constructor(product) {
     this.product = product;
   }
 
-  render() {
+  addToCart() {
+    console.log("Adding product to cart...");
+    console.log(this.product);
+  }
 
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
         <div> 
             <img src="${this.product.imageUrl}" alt="${this.product.title}">
             <div class="product-item__content">
@@ -34,43 +51,55 @@ class ProductItem {
             </div>
         </div>
       `;
-      return prodEl;
-      
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
   }
-
 }
 class ProductList {
-  products= [
+  products = [
     new Product(
       "A pillow",
       "https://www.sleepsia.in/cdn/shop/files/Microfiber_Pillow_1.jpg?v=1762335192",
       "A soft pillow!",
-      99.99
+      99.99,
     ),
     new Product(
       "A Carpet",
       "https://assets.myntassets.com/h_1440,q_75,w_1080/v1/assets/images/22866576/2025/3/12/f1f38c08-9c0a-4611-b1a5-7199a8e52c201741778724008-Storyhome-Blue--Yellow-Anti-skid-Floor-Carpet-57174177872334-1.jpg",
       "A carpet which you might like!",
-      199.99
-    )
+      199.99,
+    ),
   ];
 
   constructor() {}
 
   render() {
-    const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
 
     for (const prod of this.products) {
-    const productItem = new ProductItem(prod)
-    const prodEl = productItem.render();
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
 }
 
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
 
-const productList = new ProductList();
-productList.render(); 
+    const productList = new ProductList();
+    const productListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(productListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
