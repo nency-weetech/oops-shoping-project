ddc0a21d590c831b4e7ec25f4b4a1fa85993a251
nency rana <nency@weetechsolution.com>
2026-06-25 12:41:54 +0530

    create product class

---
+++ assets/scripts/app.js
@@ -0,0 +1,59 @@
+class Product {
+  //it's called fields
+  // title;
+  // imageUrl;
+  // description;
+  // price;
+
+  constructor(title, image, desc, price) {
+    //its called property
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+console.log(new Product());
+const productList = {
+  products: [
+    new Product(
+      "A pillow",
+      "https://www.sleepsia.in/cdn/shop/files/Microfiber_Pillow_1.jpg?v=1762335192",
+      "A soft pillow!",
+      99.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://assets.myntassets.com/h_1440,q_75,w_1080/v1/assets/images/22866576/2025/3/12/f1f38c08-9c0a-4611-b1a5-7199a8e52c201741778724008-Storyhome-Blue--Yellow-Anti-skid-Floor-Carpet-57174177872334-1.jpg",
+      "A carpet which you might like!",
+      199.99
+    )
+  ],
+
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div> 
+            <img src="${prod.imageUrl}" alt="${prod.title}">
+            <div class="product-item__content">
+                <h2>${prod.title}</h2>
+                <h3>$${prod.price}</h3>
+                <p>${prod.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+};
+
+productList.render();
